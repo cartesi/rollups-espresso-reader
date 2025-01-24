641774f37b283ea92a6c60f8440238a57915adf7
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2025-01-24 22:09:27 +0000

    Update ci.yml

--- ci/scripts/ci.sh
+++ ci/scripts/ci.sh
@@ -25,7 +25,7 @@ make generate-db
 
 echo "Build image"
 #docker build -t espresso .
-docker build -t espresso -f Dockerfile .
+docker build -t espresso -f ./ci/Dockerfile .
 
 echo "Run Anvil"
 cd rollups-node
@@ -33,13 +33,25 @@ make devnet
 make run-devnet
 cd -
 
-docker run --env-file env.nodev2-local --rm --network=host --name c_espresso espresso
-# docker run --env-file env.nodev2-sepolia --rm --network=host --name c_espresso espresso
+# export $(grep -v '^#' env.nodev2-sepolia | xargs)
+export CARTESI_BLOCKCHAIN_HTTP_ENDPOINT=https://eth-sepolia.g.alchemy.com/v2/9hjbdwjACHkHf1j01yE2j7Q9G9J1VsC9
+export CARTESI_BLOCKCHAIN_WS_ENDPOINT=wss://eth-sepolia.g.alchemy.com/v2/9hjbdwjACHkHf1j01yE2j7Q9G9J1VsC9
+
+# go test --timeout 1m -p 1 ./...
+export ESPRESSO_STARTING_BLOCK=$(curl -s https://query.decaf.testnet.espresso.network/v0/status/block-height)
+
+docker run --env-file ./ci/env.nodev2-local --rm --network=host --name c_espresso espresso
+# docker run --env-file ./ci/env.nodev2-sepolia \
+#   -e CARTESI_BLOCKCHAIN_HTTP_ENDPOINT=$CARTESI_BLOCKCHAIN_HTTP_ENDPOINT \
+#   -e CARTESI_BLOCKCHAIN_WS_ENDPOINT=$CARTESI_BLOCKCHAIN_WS_ENDPOINT \
+#   --rm --network=host --name c_espresso espresso
 
 exit 0
 
+# local anvil deploy
 docker exec c_espresso cartesi-rollups-cli app deploy -n echo-dapp -t applications/echo-dapp/ -v
 
+# sepolia deploy
 export ACCOUNT=0xB5C1674c0527b6C31A5019fD04a6C1529396DA37
 export PRIVATE_KEY=ad03...e462
 export RPC_URL=https://eth-sepolia.g.alchemy.com/v2/<key>
