43f1c82eb2eb8d1ffed8ed68952091b64a038a50
Zehui Zheng <work996mail@gmail.com>
2025-04-09 21:57:28 -0700

    merge multiple espresso tables

--- internal/repository/postgres/db/rollupsdb/espresso/table/table_use_schema.go
+++ internal/repository/postgres/db/rollupsdb/espresso/table/table_use_schema.go
@@ -10,8 +10,6 @@ package table
 // UseSchema sets a new schema name for all generated table SQL builder types. It is recommended to invoke
 // this method only once at the beginning of the program.
 func UseSchema(schema string) {
-	EspressoBlock = EspressoBlock.FromSchema(schema)
-	EspressoConfig = EspressoConfig.FromSchema(schema)
+	AppInfo = AppInfo.FromSchema(schema)
 	EspressoNonce = EspressoNonce.FromSchema(schema)
-	InputIndex = InputIndex.FromSchema(schema)
 }
--- internal/repository/postgres/schema/migrations/000001_espresso_db.up.sql
+++ internal/repository/postgres/schema/migrations/000001_espresso_db.up.sql
@@ -3,11 +3,13 @@
 
 CREATE SCHEMA espresso;
 
-CREATE TABLE IF NOT EXISTS espresso.espresso_config
+CREATE TABLE IF NOT EXISTS espresso.app_info
 (
     "application_address" ethereum_address PRIMARY KEY,
 	"starting_block" uint64 NOT NULL,
-    "namespace" uint64 NOT NULL
+    "namespace" uint64 NOT NULL,
+    "last_processed_espresso_block" uint64,
+    "index" BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS espresso.espresso_nonce
@@ -16,16 +18,4 @@ CREATE TABLE IF NOT EXISTS espresso.espresso_nonce
     "application_address" ethereum_address NOT NULL,
     "nonce" BIGINT NOT NULL,
     UNIQUE("sender_address", "application_address")
-);
-
-CREATE TABLE IF NOT EXISTS espresso.espresso_block
-(
-    "application_address" ethereum_address PRIMARY KEY,
-	"last_processed_espresso_block" uint64 NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS espresso.input_index
-(
-    "application_address" ethereum_address PRIMARY KEY,
-	"index" BIGINT NOT NULL
 );
