eb24db4f9e0e3ac434faafa426b735ff83a8b10e
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2025-05-08 12:28:46 -0300

    wip: running espresso reader with espresso dev node

--- ci/scripts/espresso-dev-node-health.sh
+++ ci/scripts/espresso-dev-node-health.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 echo "Waiting for Espresso dev node to start..."
-for i in {1..60}; do
-    # https://github.com/EspressoSystems/espresso-network/blob/985e0fb1bfdfddab509d02fb55508caa635ca550/sequencer/src/api.rs#L1926
-    if curl -sSf http://localhost:24000/healthcheck > /dev/null; then
+for i in {1..300}; do
+    if curl -sSf $ESPRESSO_BASE_URL/v1/availability/header/1 > /dev/null; then
         echo "Espresso dev node is available."
         exit 0
     else
